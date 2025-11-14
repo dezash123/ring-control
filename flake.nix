da10d9bdfd4d01dc9e5133aa45538de33c243576
dezash123 <desmondmehta@gmail.com>
2025-11-14 16:09:58 -0500

    inti

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Development environment for Ring project";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        libraries = with pkgs; [
+          webkitgtk_4_1
+          gtk3
+          cairo
+          gdk-pixbuf
+          glib
+          dbus
+          openssl
+          librsvg
+          alsa-lib
+          libclang
+          cmake
+        ];
+
+        packages = with pkgs; [
+          pkg-config
+          dbus
+          openssl
+          glib
+          gtk3
+          libsoup_3
+          webkitgtk_4_1
+          librsvg
+          alsa-lib
+          libclang
+          cmake
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = packages;
+
+          shellHook = ''
+            export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath libraries}:$LD_LIBRARY_PATH
+            export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
+          '';
+        };
+      }
+    );
+}
